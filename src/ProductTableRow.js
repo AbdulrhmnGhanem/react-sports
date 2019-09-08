764a121b810983dd164023d6b4ee60249754e59a
Abdulrhman Ghanem <abdoghanem160@gmail.com>
2019-09-08 15:26:38 +0200

    Create product row

---
+++ src/ProductTableRow.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+class ProductTableRow extends React.Component{
+
+    render() {
+        let p = this.props.products;
+        return (
+            <tr>
+                <td>{p.id}</td>
+                <td>{p.name}</td>
+                <td>{p.category}</td>
+                <td className="text-right">{Number(p.price).toFixed(2)}</td>
+                <td>
+                    <button className="btn btn-sm btn-warning m-1"
+                            onClick={() => this.props.editCallback(p)}>
+                        Edit
+                    </button>
+                    <button className="btn btn-sm btn-danger m-1"
+                            onClick={() => this.props.deleteCallback(p)}>
+                        Delete
+                    </button>
+                </td>
+            </tr>
+        )
+    }
+}
+
+export default ProductTableRow;
