05277d95f76e2114b2898199192dacc6112d8cbc
Abdulrhman Ghanem <abdoghanem160@gmail.com>
2019-09-09 07:59:45 +0200

    listing 14-10

--- src/ThemeSelector.js
+++ src/ThemeSelector.js
@@ -1,11 +1,29 @@
 import React from 'react';
 
 class ThemeSelector extends React.Component{
+
+    state = { theme: "primary" };
+
+    themes = ["primary", "secondary", "success", "warning", "dark"];
+
+    setTheme = e => { this.setState( { theme: e.target.value } ) };
+
     render() {
+        let modChildren = React.Children.map(this.props.children,
+            (child => React.cloneElement(child, { theme: this.state.theme })));
         return (
             <div className="bg-dark p-2">
+                <div className="form-group text-left">
+                    <label className="text-white">Theme:</label>
+                    <select className="form-control" value={this.state.theme}
+                            onChange={ this.setTheme }>
+                        {this.themes.map(theme =>
+                            <option key={ theme } value={ theme }>{ theme }</option>
+                        )}
+                    </select>
+                </div>
                 <div className="bg-info p-2">
-                    { this.props.children }
+                    { modChildren }
                 </div>
             </div>
         )
