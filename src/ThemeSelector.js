87f16065633bfb5b0981765fe07fd47f9c2b460a
Abdulrhman Ghanem <abdoghanem160@gmail.com>
2019-09-09 08:28:09 +0200

    listing 14-11

--- src/ThemeSelector.js
+++ src/ThemeSelector.js
@@ -2,17 +2,29 @@ import React from 'react';
 
 class ThemeSelector extends React.Component{
 
-    state = { theme: "primary" };
+    state = {
+        theme: "primary",
+        reversedChildren: false
+    };
 
     themes = ["primary", "secondary", "success", "warning", "dark"];
 
     setTheme = e => { this.setState( { theme: e.target.value } ) };
 
+    toggleReverse = () => {
+        this.setState({reversedChildren: !this.state.reversedChildren})
+    };
+
     render() {
         let modChildren = React.Children.map(this.props.children,
             (child => React.cloneElement(child, { theme: this.state.theme })));
+
+        if ( this.state.reversedChildren ) {
+            modChildren.reverse()
+        }
         return (
             <div className="bg-dark p-2">
+                <button className="btn btn-primary" onClick={ this.toggleReverse }>Reverse</button>
                 <div className="form-group text-left">
                     <label className="text-white">Theme:</label>
                     <select className="form-control" value={this.state.theme}
