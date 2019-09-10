cba87b3e1fe36e31fccdadd632b1e31f2f196f30
Abdulrhman Ghanem <abdoghanem160@gmail.com>
2019-09-10 17:18:16 +0200

    gobal Refactor

--- src/components/Selector.js
+++ src/components/Selector.js
@@ -1,38 +1,41 @@
-import React from 'react';
+import React, {Component} from "react";
 
-class Selector extends React.Component{
+class Selector extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            selection: React.Children.toArray(props.children)[0].props.name
+        }
+    }
 
-    state = { selection: React.Children.toArray(this.props.children)[0].props.name }
-
-    setSelection = e => {
-        e.persist();
-        this.setState({ selection: e.target.name })
+    setSelection = (ev) => {
+        ev.persist();
+        this.setState({selection: ev.target.name});
     }
 
     render() {
-        return (
-            <div className="container-fluid">
-                <div className="row">
-                    <div className="col-2">
-                        { React.Children.map(this.props.children, c =>
-                            <button name={ c.props.name }
-                                    onClick={ this.setSelection }
-                                    className={ `btn btn-block m-2 
-                                        ${this.state.selection === c.props.name
-                                        ? 'btn-primary active' : 'btn-secondary'}` } >
-                                { c.pros.name }
-                            </button>
-                        ) }
-                    </div>
-                    <div className="col">
-                        {
-                            React.Children.toArray(this.props.children)
-                                .filter(c => c.props.name === this.state.selection)
-                        }
-                    </div>
+        return <div className="container-fluid">
+            <div className="row">
+                <div className="col-2">
+                    {React.Children.map(this.props.children, c =>
+                        <button
+                            name={c.props.name}
+                            onClick={this.setSelection}
+                            className={`btn btn-block m-2
+                            ${this.state.selection === c.props.name
+                                ? "btn-primary active" : "btn-secondary"}`}>
+                            {c.props.name}
+                        </button>
+                    )}
+                </div>
+                <div className="col">
+                    {
+                        React.Children.toArray(this.props.children)
+                            .filter(c => c.props.name === this.state.selection)
+                    }
                 </div>
             </div>
-        )
+        </div>
     }
 }
 
