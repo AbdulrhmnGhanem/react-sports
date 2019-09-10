1c019a4f6a276e49179c968f1d10004ddd94cf85
Abdulrhman Ghanem <abdoghanem160@gmail.com>
2019-09-10 12:51:15 +0200

    Create product editor component

--- src/components/ProductEditor.js
+++ src/components/ProductEditor.js
@@ -1,71 +1,62 @@
 import React from 'react';
 
-class ProductEditor extends React.Component{
+
+class ProductEditor extends React.Component {
 
     state = {
         formData: {
-            id: this.props.id || '',
-            name: this.props.name || '',
+            id: this.props.product.id || '',
+            name: this.props.product.name || '',
             category: this.props.product.category || '',
-            price: this.props.product.price || ''
+            price: this.props.product.price || '',
         }
     };
 
-    handleChange = ev => {
-        ev.persist();
-        this.setState( state => state.formData[ev.target.name] = ev.target.value  )
+    handleChange = e => {
+        e.persist();
+        this.setState(state => state.formData[e.target.name] = e.target.value)
     };
 
-    handleClick = ev => {
+    handleClick = () => {
         this.props.saveCallback( this.state.formData )
-    }
+    };
 
     render() {
         return (
             <div className="m-2">
                 <div className="form-group">
                     <label>ID</label>
-                    <input className="form-control"
-                           name="id"
+                    <input className="form-control" name="id"
                            disabled
-                           value={this.state.formData.id}
-                           onChange={this.handleChange}
-                    />
+                           value={ this.state.formData.id }
+                           onChange={ this.handleChange } />
                 </div>
                 <div className="form-group">
                     <label>Name</label>
-                    <input className="form-control"
-                           name="name"
-                           value={this.state.formData.name}
-                           onChange={this.handleChange}
-                    />
-
+                    <input className="form-control" name="name"
+                           value={ this.state.formData.name }
+                           onChange={ this.handleChange } />
                 </div>
                 <div className="form-group">
                     <label>Category</label>
-                    <input className="form-control"
-                           name="category"
-                           value={this.state.formData.category}
-                           onChange={this.handleChange}
-                    />
-
+                    <input className="form-control" name="id"
+                           value={ this.state.formData.category }
+                           onChange={ this.handleChange } />
                 </div>
                 <div className="form-group">
                     <label>Price</label>
-                    <input className="form-control"
-                           name="price"
-                           value={this.state.formData.price}
-                           onChange={this.handleChange}
-                    />
+                    <input className="form-control" name="price"
+                           value={ this.state.formData.price }
+                           onChange={ this.handleChange } />
                 </div>
                 <div className="text-center">
                     <button className="btn btn-primary m-1"
-                            onClick={this.handleClick}>
-                        Save
+                            onClick={this.handleClick} >
+                            Save
                     </button>
-                    <button className="btn btn-secondary "
-                            onClick={this.props.cancelCallback}>
-                        Cancel
+                    <button className="btn btn-secondary"
+                            onClick={this.props.cancelCallback} >
+                            Cancel
                     </button>
                 </div>
             </div>
