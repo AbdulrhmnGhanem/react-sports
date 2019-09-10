7d356890ae41f2805cddb69b5d9da2a2a5f7c7c1
Abdulrhman Ghanem <abdoghanem160@gmail.com>
2019-09-10 12:50:38 +0200

    Create product display component

--- src/components/ProductDisplay.js
+++ src/components/ProductDisplay.js
@@ -3,59 +3,61 @@ import React from 'react';
 import ProductTable from "./ProductTable";
 import ProductEditor from "./ProductEditor";
 
-class ProductDisplay extends React.Component{
+class ProductDisplay extends React.Component {
+
     state = {
         showEditor: false,
         selectedProduct: null
     };
 
-    startEditing = product => { this.setState({ showEditor: true, selectedProduct: product }) };
-
-    createProduct = () => { this.setState( { showEditor: true, selectedProduct: {} } ) };
-
-    cancelEditing = () => { this.setState( {showEditor: false} ) };
-
-    saveProduct = product => {
-        this.props.saveCallback(product);
-        this.setState({ showEditor: false })
+    edit = product => {
+        this.setState({ showEditor: true, selectedProduct: product })
     };
 
-    renderProductEditor = () => {
+    createProduct = () => {
+        this.setState({ showEditor: true, selectedProduct: {} })
+    };
 
+    cancelEdit = () => {
+        this.setState({ showEditor: false, selectedProduct: null })
+    };
+
+    saveProduct = (product) => {
+        this.props.saveCallback(product);
+        this.setState({ showEditor: false, selectedProduct: null })
+    };
+
+    renderEditor = () => {
         return (
             <ProductEditor key={this.state.selectedProduct.id || -1}
                            product={this.state.selectedProduct}
                            saveCallback={this.saveProduct}
-                           cancelCallback={this.cancelEditing}
-            />
-        )
-    };
-
-    renderProductTable = () => {
-
-        return (
-            <div className="m-2">
-                <ProductTable products={ this.props.products }
-                              editCallback={ this.startEditing }
-                              deleteCallback={ this.props.deleteCallback }
-                />
-                <div className="text-center">
-                    <button className="btn btn-primary m-1"
-                            onClick={ this.createProduct }>
-                        Create Product
-                    </button>
-                </div>
-            </div>
+                           cancelCallback={this.cancelEdit} />
         )
     };
 
     render() {
-        if ( this.state.showEditor ) {
-            return this.renderProductEditor()
+
+        if (this.state.showEditor) {
+            return this.renderEditor()
         } else {
-            return this.renderProductTable()
+            return (
+                <div className="m-2">
+                    <ProductTable products={ this.props.product }
+                                  editCallback={ this.edit }
+                                  deleteCallback={ this.props.deleteCallback } />
+                    <div className="text-center">
+                        <button className="btn btn-primary m-1"
+                                onClick={ this.createProduct }>
+                            CreateProduct
+                        </button>
+                    </div>
+                </div>
+            )
         }
+
     }
+
 }
 
-export default ProductDisplay;
+export default ProductEditor;
