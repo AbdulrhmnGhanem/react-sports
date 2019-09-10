5d7ce4b6f1c46a0ab3106dbfba3902109096f295
Abdulrhman Ghanem <abdoghanem160@gmail.com>
2019-09-10 12:50:13 +0200

    Create product table component

--- src/components/ProductTable.js
+++ src/components/ProductTable.js
@@ -2,41 +2,37 @@ import React from 'react';
 
 import ProductTableRow from "./ProductTableRow";
 
-class ProductTable extends React.Component{
 
-    render() {
-        return(
-            <table className="table table-sm table-striped table-bordered">
-                <thead>
-                <tr>
-                    <th colSpan="5" className="bg-primary text-white text-center h4 p-2">
-                        Products
-                    </th>
-                </tr>
-                <tr>
-                    <th>ID</th><th>Name</th><th>Category</th>
-                    <th className="text-right">Price</th>
-                    <th/>
-                </tr>
-                </thead>
-                <tbody>
+const ProductTable = props => {
+    return (
+        <table className="table table-sm table-striped table-bordered">
+            <thead>
+            <tr>
+                <th colSpan="5" className="bg-primary text-center text-white h5 p-2">Products</th>
+            </tr>
+            <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Category</th>
+                <th className="text-right">Price</th>
+                <th/>
+            </tr>
+            </thead>
+            <tbody>
                 {
-                    this.props.product.map( p => {
-                        return (
-                            <ProductTableRow
-                                product={p}
-                                key={p.id}
-                                editCallback={this.props.editCallback}
-                                deleteCallback={this.props.deleteCallback}
-                            />
-                        )
-                    } )
+                    props.products.map(p =>
+                        <ProductTableRow product={p}
+                                         key={p.id}
+                                         editCallback={ props.editCallback }
+                                         deleteCallback={ props.deleteCallback }
+
+                        />)
+
+
                 }
-                </tbody>
-            </table>
-        )
-    }
+            </tbody>
+        </table>
+    )
+};
 
-}
-
-export default ProductTable;
+export default ProductTable
