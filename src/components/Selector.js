50d226d6be5f2b17cd40dd48691f3189ed0da12e
Abdulrhman Ghanem <abdoghanem160@gmail.com>
2019-09-12 15:14:18 +0200

    Indicate the active route

--- src/components/Selector.js
+++ src/components/Selector.js
@@ -1,5 +1,5 @@
 import React, {Component} from "react";
-import { BrowserRouter as Router, Link, Route, Switch, Redirect } from "react-router-dom";
+import { BrowserRouter as Router, Link, Route, Switch, Redirect, NavLink } from "react-router-dom";
 import { ProductDisplay } from "./products/ProductDisplay";
 import { SupplierDisplay } from "./suppliers/SupplierDisplay";
 
@@ -12,14 +12,20 @@ class Selector extends Component {
                 <div className="container-fluid">
                     <div className="row">
                         <div className="col-2">
-                            <div><Link to="/">Default URL</Link></div>
-                            <div><Link to="/products">Products</Link></div>
-                            <div><Link to="/suppliers">Suppliers</Link></div>
+                            <NavLink className="m-2 btn btn-block btn-primary"
+                                     activeClassName="active"
+                                     to="/">Default URL</NavLink>
+                            <NavLink className="m-2 btn btn-block btn-primary"
+                                     activeClassName="active"
+                                     to="/products" >Products</NavLink>
+                            <NavLink className="m-2 btn btn-block btn-primary"
+                                     activeClassName="active"
+                                     to="/suppliers" replace>Suppliers</NavLink>
                         </div>
                         <div className="col">
                             <Switch>
                                 <Route path="/products" component={ ProductDisplay } />
-                                <Route path='/suppliers' component={ SupplierDisplay } />
+                                <Route path='/suppliers' component={ SupplierDisplay }  />
                                 <Redirect to="/products"/>
                             </Switch>
                         </div>
