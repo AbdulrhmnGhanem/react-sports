69f7130d76fe6722fff84059e82f223e38a4589b
Abdulrhman Ghanem <abdoghanem160@gmail.com>
2019-09-12 09:44:44 +0200

    Add basic router

--- src/components/Selector.js
+++ src/components/Selector.js
@@ -1,42 +1,29 @@
 import React, {Component} from "react";
+import { BrowserRouter as Router, Link, Route } from "react-router-dom";
+import { ProductDisplay } from "./products/ProductDisplay";
+import { SupplierDisplay } from "./suppliers/SupplierDisplay";
+
 
 class Selector extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            selection: React.Children.toArray(props.children)[0].props.name
-        }
-    }
 
-    setSelection = (ev) => {
-        ev.persist();
-        this.setState({selection: ev.target.name});
-    }
 
     render() {
-        return <div className="container-fluid">
-            <div className="row">
-                <div className="col-2">
-                    {React.Children.map(this.props.children, c =>
-                        <button
-                            name={c.props.name}
-                            onClick={this.setSelection}
-                            className={`btn btn-block m-2
-                            ${this.state.selection === c.props.name
-                                ? "btn-primary active" : "btn-secondary"}`}>
-                            {c.props.name}
-                        </button>
-                    )}
+        return(
+            <Router>
+                <div className="container-fluid">
+                    <div className="row">
+                        <div className="col-2">
+                            <div><Link to="/products">Products</Link></div>
+                            <div><Link to="/suppliers">Suppliers</Link></div>
+                        </div>
+                        <div className="col">
+                            <Route path="/products" component={ ProductDisplay }/>
+                            <Route path="/suppliers" component={ SupplierDisplay }/>
+                        </div>
+                    </div>
                 </div>
-                <div className="col">
-                    {
-                        React.Children.toArray(this.props.children)
-                            .filter(c => c.props.name === this.state.selection)
-                    }
-                </div>
-            </div>
-        </div>
-    }
+            </Router>
+        )}
 }
 
 export default Selector;
