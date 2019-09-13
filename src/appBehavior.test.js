d1bea0c01c21dc77aa64bdcf7add5e346029dc6d
Abdulrhman Ghanem <abdoghanem160@gmail.com>
2019-09-13 21:00:55 +0200

    Test: the effect of methods

--- src/appBehavior.test.js
+++ src/appBehavior.test.js
@@ -16,4 +16,13 @@ it('uses title prop', () => {
     expect(stateValue).toBe(titleVal);
 });
 
+it('updates state data', () => {
+    const wrapper = shallow(<App />);
+    const values = [10, 20, 30];
+    values.forEach((val, index) =>
+        wrapper.instance().updateFieldValue(index + 1, val));
+    wrapper.instance().updateTotal();
 
+    expect(wrapper.state("total"))
+        .toBe(values.reduce((total, val) => total + val), 0);
+});
