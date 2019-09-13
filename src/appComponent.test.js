1e39ab51873e72a09e0e573c7bb39cf7863046e9
Abdulrhman Ghanem <abdoghanem160@gmail.com>
2019-09-13 20:38:04 +0200

    Test: full render

--- src/appComponent.test.js
+++ src/appComponent.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Adapter from 'enzyme-adapter-react-16';
-import Enzyme, { shallow } from 'enzyme';
+import Enzyme, { shallow, mount } from 'enzyme';
 import App from "./App";
 import ValueInput from "./ValueInput";
 
@@ -11,3 +11,16 @@ it('should Render three value input', () => {
     const valCount = wrapper.find(ValueInput).length;
     expect(valCount).toBe(3)
 });
+
+it("should fully renders three inputs", () => {
+    const wrapper = mount(<App title="tester" />);
+    const count = wrapper.find("input.form-control").length;
+    expect(count).toBe(3);
+});
+
+it('should Shallow render zero inputs', function () {
+    const wrapper = shallow(<App />);
+    const count = wrapper.find("input.form-control").length;
+    expect(count).toBe(0)
+    
+}); 
